3bfe589acbd19b20f38859961dea5d2950ab1e95
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-11-06 18:15:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sprocketsissue::Application.config.secret_token = '565447516e4063e8807816b42cec454bc5676e0130410e4edd611253635f290adb3e74ee28b08110a8a28fa701347590d60bf5ef547b0e61586870b1bcd0a85e'
